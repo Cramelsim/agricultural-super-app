08aa6be96e216f2080bb3fb70193abd5ca3da6ac
Melody Simwa <simwamelody@gmail.com>
2026-01-08 17:02:15 +0300

    store.js imports

---
+++ frontend/src/store/store.js
@@ -0,0 +1,6 @@
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './slices/authSlice';
+import postReducer from './slices/postSlice';
+import userReducer from './slices/userSlice';
+import messageReducer from './slices/messageSlice';
+import communityReducer from './slices/communitySlice';
