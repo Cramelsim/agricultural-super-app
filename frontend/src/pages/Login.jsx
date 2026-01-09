dd16eed6964270d652d0b0bf8e6555a669e3d8c7
Melody Simwa <simwamelody@gmail.com>
2026-01-09 17:33:54 +0300

    login.jsx

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, clearError } from '../store/slices/authSlice';
+import {
+  Container,
+  Paper,
+  Typography,
+  TextField,
+  Button,
+  Box,
+  Alert,
+  CircularProgress,
+  Grid,
+} from '@mui/material';
+import { styled } from '@mui/material/styles';
