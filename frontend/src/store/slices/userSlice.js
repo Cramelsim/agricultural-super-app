f629be47806b4ff2ccd613f1ea2a91c05aa08754
Melody Simwa <simwamelody@gmail.com>
2026-01-12 19:55:43 +0300

    add const get  user slice

--- frontend/src/store/slices/userSlice.js
+++ frontend/src/store/slices/userSlice.js
@@ -1,2 +1,14 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import api from '../../services/api';
+
+export const getUser = createAsyncThunk(
+  'users/getUser',
+  async (userId, { rejectWithValue }) => {
+    try {
+      const response = await api.get(`/users/${userId}`);
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.response?.data || error.message);
+    }
+  }
+);
