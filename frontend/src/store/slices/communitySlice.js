455998096bc487f2fafe59af0f10ea7f558d38cf
Melody Simwa <simwamelody@gmail.com>
2026-01-14 15:02:38 +0300

    add get communities in community slice

--- frontend/src/store/slices/communitySlice.js
+++ frontend/src/store/slices/communitySlice.js
@@ -1,2 +1,14 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import api from '../../services/api';
+
+export const getCommunities = createAsyncThunk(
+  'communities/getCommunities',
+  async (params, { rejectWithValue }) => {
+    try {
+      const response = await api.get('/communities', { params });
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.response?.data || error.message);
+    }
+  }
+);
