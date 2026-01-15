80087c8a7c66f494d7974af98b2fb7b70dcb0a71
Melody Simwa <simwamelody@gmail.com>
2026-01-15 22:04:58 +0300

    add get conversations constant in message slice

--- frontend/src/store/slices/messageSlice.js
+++ frontend/src/store/slices/messageSlice.js
@@ -1,2 +1,14 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import api from '../../services/api';
+
+export const getConversations = createAsyncThunk(
+  'messages/getConversations',
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await api.get('/messages/conversations');
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.response?.data || error.message);
+    }
+  }
+);
