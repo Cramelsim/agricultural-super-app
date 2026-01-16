4867621d7ab13e3954e06468ba306a6a4c25c712
Melody Simwa <simwamelody@gmail.com>
2026-01-16 17:56:11 +0300

    add imports in community page

---
+++ frontend/src/pages/Community.jsx
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import {
+  Container,
+  Grid,
+  Card,
+  CardContent,
+  CardMedia,
+  CardActions,
+  Typography,
+  Button,
+  TextField,
+  Box,
+  Chip,
+  CircularProgress,
+  Alert,
+  IconButton,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+} from '@mui/material';
