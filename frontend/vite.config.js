80f3dc1a936cd375df1c636d056339928dcf1046
Melody Simwa <simwamelody@gmail.com>
2026-01-17 15:50:47 +0300

    update config.jsn

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,3 +1,4 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -5,5 +6,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+    open: true  // Automatically open browser
   }
 })
