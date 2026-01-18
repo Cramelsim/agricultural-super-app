0500d81d90f8c6c6769938a143df284151ac71ab
Melody Simwa <simwamelody@gmail.com>
2026-01-18 14:52:02 +0300

    update app.jsx profile import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ import Layout from './components/Layout';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import Profile from './pages/Profile;
+import Profile from './pages/Profile';
 import FeedPage from './pages/FeedPage';
 import ExplorePage from './pages/ExplorePage';
 import CommunitiesPage from './pages/CommunitiesPage';
