2031c97b5a910fa4825f234a4fb561d4ce89dd53
Melody Simwa <simwamelody@gmail.com>
2026-01-21 16:16:49 +0300

    imports in explore page

---
+++ frontend/src/pages/Explore.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import {
+  Container,
+  Grid,
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+  Box,
+  TextField,
+  InputAdornment,
+  Chip,
+  Button,
+  Avatar,
+  IconButton,
+} from '@mui/material';
+import { Search, LocationOn, Star, StarBorder } from '@mui/icons-material';
+import { Link } from 'react-router-dom';
