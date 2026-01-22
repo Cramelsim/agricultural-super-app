b5a0dfd55087f0fa9893c6cf92631f42dae9183d
Melody Simwa <simwamelody@gmail.com>
2026-01-22 15:57:17 +0300

    import explore page

---
+++ frontend/src/pages/Homepage.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import { Container, Grid, Box, Typography, Button, Card, CardContent } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { Agriculture, TrendingUp, Group, Forum } from '@mui/icons-material';
