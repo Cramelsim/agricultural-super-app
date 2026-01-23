89aeb2ca690046c7c14baa8ae50aa718546be7b9
Melody Simwa <simwamelody@gmail.com>
2026-01-23 17:15:49 +0300

    messages import

---
+++ frontend/src/pages/Messages.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import {
+  Container,
+  Grid,
+  Paper,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Avatar,
+  Typography,
+  Box,
+  TextField,
+  Button,
+  IconButton,
+  Badge,
+} from '@mui/material';
+import { Send, Search } from '@mui/icons-material';
