5f1be2480940f32078f4861b769d4727fce4b221
Melody Simwa <simwamelody@gmail.com>
2026-01-25 13:04:50 +0300

    import profile  page

---
+++ frontend/src/pages/Profile.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import {
+  Container,
+  Grid,
+  Paper,
+  Avatar,
+  Typography,
+  Box,
+  Button,
+  Tabs,
+  Tab,
+  Card,
+  CardContent,
+  CardMedia,
+  IconButton,
+  Chip,
+} from '@mui/material';
+import { Edit, LocationOn, Email, Phone, Agriculture } from '@mui/icons-material';
