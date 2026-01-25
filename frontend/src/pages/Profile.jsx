b6f259b29b366bda8e6ba0b56b833bb3b39b9cc2
Melody Simwa <simwamelody@gmail.com>
2026-01-25 13:28:44 +0300

    tabs

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -128,4 +128,14 @@ Grid container spacing={2} sx={{ mb: 3 }}>
             </Grid>
           </Grid>
         </Grid>
+
       </Paper>
+        {/* Tabs */}
+      <Box sx={{ borderBottom: 1, borderColor: 'divider', mb: 3 }}>
+        <Tabs value={tabValue} onChange={handleTabChange}>
+          <Tab label="Posts" />
+          <Tab label="Comments" />
+          <Tab label="Saved" />
+          <Tab label="Communities" />
+        </Tabs>
+      </Box>
