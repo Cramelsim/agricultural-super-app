b0237ec1fcfebde5472bdda23b596cf8864ac10f
Melody Simwa <simwamelody@gmail.com>
2026-01-26 16:00:08 +0300

    imports in register page

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import {
+  Container,
+  Paper,
+  TextField,
+  Button,
+  Typography,
+  Box,
+  Link,
+  Grid,
+  FormControlLabel,
+  Checkbox,
+  Alert,
+  InputAdornment,
+  IconButton,
+} from '@mui/material';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
+import { Link as RouterLink } from 'react-router-dom';
