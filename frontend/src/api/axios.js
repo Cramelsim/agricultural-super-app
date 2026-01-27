e1ee58cfd732150db9640f909e5a6190088f49e4
Melody Simwa <simwamelody@gmail.com>
2026-01-27 15:55:27 +0300

    add axios.js code in api

---
+++ frontend/src/api/axios.js
@@ -0,0 +1,20 @@
+// src/api/axios.js
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://localhost:5000/api', // Your Flask backend URL
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// Add token to requests if available
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default api;
