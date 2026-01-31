e2e7e3a0527ea02a6cbdcc9155d5bf75f0f7d4f5
Melody Simwa <simwamelody@gmail.com>
2026-01-31 17:36:43 +0300

    import useState

--- frontend/src/pages/Feed.jsx
+++ frontend/src/pages/Feed.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { likePost, getPosts } from '../store/slices/postSlice';
+
 import {
   Container,
   Grid,
