e38f021567d6e1389f0f3a3bd1ee06c9cdb0f226
Melody Simwa <simwamelody@gmail.com>
2026-02-06 16:31:14 +0300

    add user model

--- backend/app/models.py
+++ backend/app/models.py
@@ -3,7 +3,7 @@ from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
 import uuid
 
-
+class User(db.Model):
     __tablename__ = 'users'
     
     id = db.Column(db.Integer, primary_key=True)
