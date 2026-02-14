ea83c77093ab5532bcfbd95c127b4235ab004442
Melody Simwa <simwamelody@gmail.com>
2026-02-14 20:14:21 +0300

    import database

--- backend/app/routes/communities.py
+++ backend/app/routes/communities.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, jsonify, current_app
 from flask_jwt_extended import jwt_required, get_jwt_identity
-from app import db
 
+from app.models import User, Community, CommunityMember, Post
 
 communities_bp = Blueprint('communities', __name__)
 
