defce35ff2628e40d1c6dfd976303744d3216623
Melody Simwa <simwamelody@gmail.com>
2026-02-15 19:35:38 +0300

    register followss blueprint

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -36,7 +36,7 @@ def create_app(config_class=Config):
     from app.routes.posts import posts_bp
     from app.routes.comments import comments_bp
     from app.routes.messages import messages_bp
-  
+    from app.routes.follows import follows_bp
     from app.routes.communities import communities_bp
     from app.routes.explore import explore_bp
 
