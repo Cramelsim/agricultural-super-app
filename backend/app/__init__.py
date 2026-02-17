32ccb6ce85840ed1da55eb38eb616522ced8f560
Melody Simwa <simwamelody@gmail.com>
2026-02-17 18:36:07 +0300

    register authentiction blurprint

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -41,7 +41,7 @@ def create_app(config_class=Config):
     from app.routes.explore import explore_bp
 
     
-    app.register_blueprint(auth_bp, url_prefix='/api/auth')
+   
     app.register_blueprint(users_bp, url_prefix='/api/users')
     app.register_blueprint(posts_bp, url_prefix='/api/posts')
     app.register_blueprint(comments_bp, url_prefix='/api/comments')
