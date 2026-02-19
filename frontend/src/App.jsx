170d66c38a6834f069052da92e3d8c568a5ddc05
Melody Simwa <simwamelody@gmail.com>
2026-02-19 23:01:05 +0300

    register profile route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -61,7 +61,7 @@ function App() {
               <Route path="community" element={<Community />} />
               <Route path="messages" element={<Messages/>} />
               <Route path="profile/:id" element={<Profile />} />
-             
+              <Route path="profile" element={<Profile />} />
               <Route path="create-post" element={<CreatePost />} />
               <Route path="post/:id" element={<PostDetail />} />
             </Route>
