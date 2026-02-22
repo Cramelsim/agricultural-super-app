4b64d1140b90575a40d1cd9e3df5e236e6c77cb4
Melody Simwa <simwamelody@gmail.com>
2026-02-22 23:05:45 +0300

    register home page route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -53,7 +53,7 @@ function App() {
         <Router>
           <Routes>
             <Route path="/" element={<Layout />}>
-              <Route index element={<Homepage />} />
+            
               <Route path="login" element={<Login/>} />
               <Route path="register" element={<Register />} />
               <Route path="feed" element={<Feed />} />
