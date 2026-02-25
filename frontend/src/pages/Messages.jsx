dbffbbd12ad25b733317801e3cbf39ee99811dcd
Melody Simwa <simwamelody@gmail.com>
2026-02-25 23:01:16 +0300

    get initials constant

--- frontend/src/pages/Messages.jsx
+++ frontend/src/pages/Messages.jsx
@@ -196,7 +196,8 @@ const MessagesPage = () => {
                         backgroundColor: 'primary.light',
                       },
                     }}
-                  
+                  >
+
                     <ListItemAvatar>
                       <Badge
                         badgeContent={conversation.unread_count || 0}
