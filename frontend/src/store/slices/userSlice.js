02a11e89bbc66fe140f48b563faa4857d9d10809
Melody Simwa <simwamelody@gmail.com>
2026-02-27 22:07:14 +0300

    get folllowers constant

--- frontend/src/store/slices/userSlice.js
+++ frontend/src/store/slices/userSlice.js
@@ -87,7 +87,17 @@ export const getFollowing = createAsyncThunk(
   }
 );
 
-
+export const getFollowers = createAsyncThunk(
+  'users/getFollowers',
+  async (params, { rejectWithValue }) => {
+    try {
+      const response = await api.get('/follows/followers', { params });
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.response?.data || error.message);
+    }
+  }
+);
 
 const initialState = {
   currentUser: null,
