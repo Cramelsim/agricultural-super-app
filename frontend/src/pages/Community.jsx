62365edcfe4290cffad29aab28b63be85e44c8d8
Melody Simwa <simwamelody@gmail.com>
2026-03-08 23:09:49 +0300

    choose image button

--- frontend/src/pages/Community.jsx
+++ frontend/src/pages/Community.jsx
@@ -485,7 +485,10 @@ const CommunitiesPage = () => {
               onChange={handleImageChange}
             />
             <label htmlFor="community-image-upload">
-              
+              <Button variant="outlined" component="span">
+                Choose Image
+              </Button>
+            </label>
             {newCommunity.image && (
               <Typography variant="caption" sx={{ ml: 2 }}>
                 {newCommunity.image.name}
